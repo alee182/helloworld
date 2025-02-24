a7d95ebcc8db8cdb84da21d81fa7a6d3eda577e8
alee182 <146772987+alee182@users.noreply.github.com>
2025-02-23 22:26:52 -0500

    custom user model creation complete

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
-from .models import Room, Topic, Message
 
-admin.site.register(Room)
-admin.site.register(Topic)
-admin.site.register(Message)
+admin.site.register(User)
